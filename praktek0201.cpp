cde5d66db0435516682ad8276fc08b07ec0fd9f2
hilmanhamidi <hilmanhamidi007@gmail.com>
2017-10-25 16:16:26 +0800

    "membuat file praktek0202.cpp"

--- praktek0201.cpp
+++ praktek0201.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    nilai=nilai+1;
+int main() {
     int nilai;
-    int 1nilai;
+    int Nilai = 1;
+    
+    cout<< "masukkan nilai = ";
+    cin>>nilai;
+    nilai=nilai+Nilai;
     
     cout<<nilai;
-    cout<<1nilai;
 }
---
+++ praktek0204.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+        using namespace std;
+int main(){
+	    double luasrumah=42;
+	    double lantaipkt=193*1215;
+	    double luas1box=lantaipkt*8/1000000;
+	    double jumlahboxp,biayap,biayapg,total18;(
+		
+		cout<<"luas rumah               :"<<luasrumah<<"m2"<<endl;
+		cout<<"luas 1 box               :"<<luas1box<<"m2"<<endl;
+		
+		jumlahboxp = luasrumah/luasbox;
+		cout<<"jumlah box parket        :"<<jumlahboxp<<endl;
+		
+		biayap = jumlahboxp*500;
+		cout<<"biaya parket (rp)        :"<<biayap<<endl;
+		
+		biayapg = luasrumah*20;
+		cout<<"biaya pasang (rp)        :"<<biayapg<<endl;
+		
+		totalb = biayap+biayapg;
+		cout<<"total biaya (rp)         :"<<totalb<<endl;
+		)
+}
